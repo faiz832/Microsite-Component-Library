b83391cb0f1cce76ab2e93fc93108280c2047efb
faiz832 <faqodefazz@gmail.com>
2024-11-27 18:33:41 +0700

    update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,13 +4,21 @@
     <div class="">
         <div class="p-6 rounded-md border border-gray-200 dark:border-gray-800">
             @role('admin')
-                <h1 class="text-2xl font-semibold text-gray-900 dark:text-gray-200 leading-tight">
-                    {{ __('Admin Dashboard') }}
-                </h1>
+                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                    {{ __('Welcome Back Developer!') }}
+                </h2>
+        
+                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                    {{ __("You are logged in as an admin.") }}
+                </p>
             @else
-                <h1 class="text-2xl font-semibold text-gray-900 dark:text-gray-200 leading-tight">
-                    {{ __('User Dashboard') }}
-                </h1>
+                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                    {{ __('Welcome Back Developer!') }}
+                </h2>
+        
+                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                    {{ __("You are logged in as a user.") }}
+                </p>
             @endrole
         </div>
     </div>

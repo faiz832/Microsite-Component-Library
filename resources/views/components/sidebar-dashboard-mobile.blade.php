e7f5257366c55f505455f869c9f9ca48c963ca59
faiz832 <faqodefazz@gmail.com>
2024-11-27 18:53:30 +0700

    fix dashboard sidebar mobile

--- resources/views/components/sidebar-dashboard-mobile.blade.php
+++ resources/views/components/sidebar-dashboard-mobile.blade.php
@@ -87,7 +87,8 @@
                                 </svg>
                                 Component
                             </a>
-                            <a href="#" class="flex items-center px-2 py-2 text-sm rounded-md text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white">
+                            <a href="{{route('category.index')}}" class="{{Route::is('category.index') ? 'font-semibold dark:text-white' : ''}} relative flex items-center px-2 py-2 text-sm rounded-md text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white">
+                                <div class="{{Route::is('category.index') ? 'block' : 'hidden'}} absolute inset-y-0 -left-2 w-1 h-6 my-auto bg-purple-600 rounded-t-lg rounded-b-lg"></div>
                                 <svg class="mr-3 h-5 w-5 text-gray-700 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"></path>
                                 </svg>

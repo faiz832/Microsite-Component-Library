b207c8eb55e9cb583333c7477d9aeff15b032ccc
faiz832 <faqodefazz@gmail.com>
2024-12-02 11:05:21 +0700

    fix title edit component

--- resources/views/component/edit.blade.php
+++ resources/views/component/edit.blade.php
@@ -6,7 +6,7 @@
             <div class="">
                 <header>
                     <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                        {{ __('Create New component') }}
+                        {{ __('Edit Existing component') }}
                     </h2>
 
                     <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
